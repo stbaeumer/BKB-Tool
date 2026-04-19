ad8c676c4ed1b0c509c2b7d75a593a643650d5a9
stbaeumer <baeumer@posteo.ed>
2026-04-19 16:31:57 +0200

    ...

--- BKB-Tool/Program.cs
+++ BKB-Tool/Program.cs
@@ -187,6 +187,7 @@ IConfiguration CheckForUpdate(IConfiguration configuration)
                         return configuration;
                     }
 
+
                     // 2) Zielpfade bestimmen
                     string appImagePath = Environment.GetEnvironmentVariable("APPIMAGE");
                     if (string.IsNullOrEmpty(appImagePath))
